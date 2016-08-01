da97be31797cc4273c4f356acd87460da287c71c
jetsonhacks <jim@jetsonhacks.com>
2016-07-31 22:58:46 -0700

    Add installJetsonCar script

---
+++ installJetsonCar.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Install the Jetson Car support files
+# This includes the code for the Arduino Car interface
+# The packages needed for installing ROS on an Arduino
+# Joystick support for the Nyko game controller
+# This repository
+# git clone https://github.com/jetsonhacks/jetsoncar.git
+
+# Make the jetsonbot catkin workspace
+./setupCatkinWorkspace.sh jetsonbot
+cd ~/jetsonbot
+sudo apt-get install arduino
+sudo apt-get install ros-indigo-joy -y
+cd src
+git clone https://github.com/jetsonhacks/jetsoncar_teleop.git
+cd ..
+catkin_make
+
+# Copy Arduino code 
+cd ~/jetsoncar
+cp -r Arduino\ Firmware/* '/home/ubuntu/sketchbook'
+sudo apt-get install ros-indigo-rosserial-arduino ros-indigo-rosserial -y
+cd ~/sketchbook/libraries
+rm -rf ros_lib
+source ~/jetsonbot/devel/setup.bash
+rosrun rosserial_arduino make_libraries.py ~/sketchbook/libraries
+cd ~/jetsoncar
+
